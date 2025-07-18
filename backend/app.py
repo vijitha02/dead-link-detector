d3ed8e8be122bb724cab00798013d43de2494a54
vijitha02 <vijitharohini90@gmail.com>
2025-07-18 15:40:53 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -73,4 +73,4 @@ from routes.auth import *
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
